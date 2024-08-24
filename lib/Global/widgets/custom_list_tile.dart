478784428ec1fd0b75c6e1005e29428189ca8cea
DIVYAKUMAR PATEL <124331485+Divyakumar21202@users.noreply.github.com>
2024-08-24 19:57:09 +0530

    Chore: uiUpdate has been merged and tested locally

--- lib/Global/widgets/custom_list_tile.dart
+++ lib/Global/widgets/custom_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mess_mgmt/Global/models/coupon_data_model.dart';
+import 'package:mess_mgmt/Global/widgets/my_list_tile.dart';
 
 class GlassyListTile extends StatelessWidget {
   final CouponDataModel coupon;
@@ -10,68 +11,73 @@ class GlassyListTile extends StatelessWidget {
     required this.coupon,
     required this.i,
   });
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        color: Colors.blueAccent,
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.2),
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(
-              color: Colors.white.withOpacity(0.3),
-              width: 1.5,
-            ),
-          ),
-          child: ListTile(
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            title: Text(
-              coupon.createdBy?.fName ?? '',
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            subtitle: Text(
-              "Floor: ${coupon.couponFloor}",
-              style: const TextStyle(
-                color: Colors.white70,
-              ),
-            ),
-            trailing: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Rs ${coupon.price}",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  coupon.isVeg ? 'Veg' : 'Non-Veg',
-                  style: const TextStyle(
-                    color: Colors.white70,
-                    fontSize: 14,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return MyListTile(coupon: coupon);
   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(8.0),
+//       child: Card(
+//         color: Colors.blueAccent,
+//         elevation: 5,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(20),
+//         ),
+//         child: Container(
+//           decoration: BoxDecoration(
+//             color: Colors.white.withOpacity(0.2),
+//             borderRadius: BorderRadius.circular(20),
+//             border: Border.all(
+//               color: Colors.white.withOpacity(0.3),
+//               width: 1.5,
+//             ),
+//           ),
+//           child: ListTile(
+//             contentPadding:
+//                 const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+//             title: Text(
+//               coupon.createdBy?.fName ?? '',
+//               style: const TextStyle(
+//                 color: Colors.white,
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 18,
+//               ),
+//             ),
+//             subtitle: Text(
+//               "Floor: ${coupon.couponFloor}",
+//               style: const TextStyle(
+//                 color: Colors.white70,
+//               ),
+//             ),
+//             trailing: Column(
+//               crossAxisAlignment: CrossAxisAlignment.end,
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Text(
+//                   "Rs ${coupon.price}",
+//                   style: const TextStyle(
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.bold,
+//                     fontSize: 16,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 5),
+//                 Text(
+//                   coupon.isVeg ? 'Veg' : 'Non-Veg',
+//                   style: const TextStyle(
+//                     color: Colors.white70,
+//                     fontSize: 14,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 }
--- lib/features/dashboard/screens/view_screen.dart
+++ lib/features/dashboard/screens/view_screen.dart
@@ -4,11 +4,11 @@ import 'package:mess_mgmt/Global/enums/enums.dart';
 import 'package:mess_mgmt/Global/enums/pagination_enum.dart';
 import 'package:mess_mgmt/Global/widgets/custom_filter_dialog.dart';
 import 'package:mess_mgmt/Global/widgets/custom_list_tile.dart';
+import 'package:mess_mgmt/Global/widgets/custom_shimmerlist_tile.dart';
 import 'package:mess_mgmt/Global/widgets/loader.dart';
 import 'package:mess_mgmt/features/dashboard/stores/dashboard_store.dart';
 
 import '../../../Global/Error Screen/network_error_screen.dart';
-import '../../../Global/effects/shimmer_effect.dart';
 
 class ViewScreen extends StatelessWidget {
   const ViewScreen({super.key});
@@ -24,11 +24,8 @@ class ViewScreen extends StatelessWidget {
             onPressed: () {
               showFilterDialog(context: context);
             },
-            label: const Text('Apply Filter',style: TextStyle(color: Colors.white),),
-            icon: const Icon(
-              Icons.filter_list,
-              color: Colors.white,
-            ),
+            label: const Text('Apply Filter'),
+            icon: const Icon(Icons.filter_list),
           ),
         ],
       ),
@@ -52,18 +49,20 @@ class ViewScreen extends StatelessWidget {
               children: [
                 for (int i = 0; i < 3; i++) ...[
                   const SizedBox(height: 16),
-                  ShimmerEffect(
-                    child: Card(
-                      margin: const EdgeInsets.symmetric(horizontal: 16),
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                      child: const Padding(
-                        padding: EdgeInsets.all(16),
-                        child: SizedBox(height: 100, width: double.infinity),
-                      ),
-                    ),
-                  ),
+                  // ShimmerEffect(
+                  //   child: Card(
+                  //     margin: const EdgeInsets.symmetric(horizontal: 16),
+                  //     elevation: 4,
+                  //     shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(12)),
+                  //     child: const Padding(
+                  //       padding: EdgeInsets.all(16),
+                  //       child: SizedBox(height: 100, width: double.infinity),
+                  //     ),
+                  //   ),
+                  // ),
+                  const ShimmerListTile(),
+
                   const SizedBox(height: 16),
                 ]
               ],
@@ -90,7 +89,7 @@ class ViewScreen extends StatelessWidget {
                         i: index,
                       ),
                       if (!isPaginationLoading)
-                        pagination.getPaginationwidget(onPressed: (){
+                        pagination.getPaginationwidget(onPressed: () {
                           dashboardStore.skipLoadMoreData();
                         }),
                       if (isPaginationLoading) const AppLoader()
